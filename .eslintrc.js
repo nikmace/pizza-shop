fd1247a6a9f5bcfb6618ed8db9a17b4fe47e4b31
Nikita Baranov <nikmacentosh@gmail.com>
2022-05-26 19:35:11 +0300

    Updated config and packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,7 +55,7 @@ module.exports = {
     ],
     'no-invalid-this': 'error',
     'no-new-wrappers': 'error',
-    'no-param-reassign': 'error',
+    'no-param-reassign': 'off',
     'no-redeclare': 'error',
     'no-sequences': 'error',
     'no-shadow': [
