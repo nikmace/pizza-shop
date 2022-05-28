39eff90d806a0a69eb992133547335ef42e3363a
Nikita Baranov <nikmacentosh@gmail.com>
2022-05-28 14:03:52 +0300

    Added CartItem type

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -8,3 +8,15 @@ export interface IPizza {
   category: number;
   rating: number;
 }
+
+export type CartItem = {
+  id: string;
+  imageUrl: string;
+  name: string;
+  type: string;
+  size: number;
+  price: number;
+  category: number;
+  rating: number;
+  count: number;
+};
