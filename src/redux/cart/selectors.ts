fb61cbe35fc43ec3edfa52ec54c7fbe7a9aac0eb
Nikita Baranov <nikmacentosh@gmail.com>
2022-05-29 15:16:17 +0300

    Updated redux logic to include pizza variations

--- src/redux/cart/selectors.ts
+++ src/redux/cart/selectors.ts
@@ -4,6 +4,21 @@ import { RootState } from 'redux/store';
 export const selectCart = (state: RootState) => state.cart.cartItems;
 export const selectTotalPrice = (state: RootState) => state.cart.totalPrice;
 export const selectCartItemById = (id: string) => (state: RootState) =>
-  state.cart.cartItems.find((obj) => obj.id === id);
-export const selectCartItemsCount = (state: RootState) =>
-  state.cart.cartItems.reduce((sum, item) => sum + item.count, 0);
+  state.cart.cartItems.reduce((sum, item) => {
+    if (item.id === id) {
+      sum += item.variation.count;
+    }
+    return sum;
+  }, 0);
+
+export const selectCartItemsCount = (state: RootState): number =>
+  state.cart.cartItems.reduce((sum, item) => sum + item.variation.count, 0);
+
+export const selectVariationTotalPrice =
+  (id: string, variationId: string) => (state: RootState) =>
+    state.cart.cartItems.reduce((sum, item) => {
+      if (item.variation.id === variationId && item.id === id) {
+        sum += item.price * item.variation.count;
+      }
+      return sum;
+    }, 0);
--- src/redux/cart/slice.ts
+++ src/redux/cart/slice.ts
@@ -1,4 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+// import pizzaVariations from 'pizza-variations';
 import { CartItem } from 'types/types';
 import calcTotalPrice from 'utils/calcTotalPrice';
 import getCartFromLS from 'utils/getCartFromLS';
@@ -17,39 +19,81 @@ export const cartSlice = createSlice({
     addItemToCart: (state, action: PayloadAction<CartItem>) => {
       // Look if there is already a pizza with same id added to cart
       const itemWithSameId = state.cartItems.find(
-        (item) => item.id === action.payload.id
+        (item: CartItem) =>
+          item.id === action.payload.id &&
+          item.variation.id === action.payload.variation.id
       );
 
       // If that pizza exist we just add +1 to the count
-      if (itemWithSameId) {
-        itemWithSameId.count += 1;
+      if (itemWithSameId && itemWithSameId.variation.count > 0) {
+        itemWithSameId.variation.count += 1;
       } else {
-        const order = { ...action.payload, count: 1 };
+        // eslint-disable-next-line prefer-const
+        const order: CartItem = { ...action.payload };
+
+        order.variation = {
+          ...order.variation,
+          count: 1,
+        };
 
         state.cartItems.push(order);
       }
 
+      // console.log(`Count of items in cart: ${state.cartItems.length}`);
       state.totalPrice = calcTotalPrice(state.cartItems);
+      localStorage.setItem('cart', JSON.stringify(state.cartItems));
     },
-    minusItem: (state, action: PayloadAction<string>) => {
-      // Find the item with that id in the cart
-      const itemWithSameId = state.cartItems.find(
-        (item) => item.id === action.payload
-      );
+    minusItem: (
+      state,
+      action: PayloadAction<{ id: string; variationId: string }>
+    ) => {
+      // Find the item with that variationId in the cart
+      let elIdx = 0;
+      const itemWithSameId = state.cartItems.find((item: CartItem, idx) => {
+        elIdx = idx;
+        return (
+          item.variation.id === action.payload.variationId &&
+          item.id === action.payload.id
+        );
+      });
 
+      // Minus the count
       if (itemWithSameId) {
-        itemWithSameId.count -= 1;
+        itemWithSameId.variation.count -= 1;
       }
 
-      state.totalPrice = calcTotalPrice(state.cartItems);
-    },
-    removeSingleItem: (state, action: PayloadAction<string>) => {
-      // Find the item with that id in the cart
-      state.cartItems = state.cartItems.filter(
-        (obj) => obj.id !== action.payload
-      );
+      // When the item with that id and variation
+      // reaches 0 count we remove it from the array
+      if (itemWithSameId?.variation.count === 0) {
+        state.cartItems.splice(elIdx, 1);
+      }
+
       // Recalculate total price
       state.totalPrice = calcTotalPrice(state.cartItems);
+      // Update localStorage with newest cartItems
+      localStorage.setItem('cart', JSON.stringify(state.cartItems));
+    },
+    removeSingleItem: (
+      state,
+      action: PayloadAction<{ id: string; variationId: string }>
+    ) => {
+      // Find the item with that variationId in the cart
+      const idx = state.cartItems.findIndex(
+        (item) =>
+          item.variation.id === action.payload.variationId &&
+          item.id === action.payload.id
+      );
+
+      if (idx < 0) {
+        throw new Error('Item with that index does not exist');
+      }
+
+      state.cartItems.splice(idx, 1);
+
+      // Recalculate total price
+      state.totalPrice = calcTotalPrice(state.cartItems);
+      // Update localStorage with newest cartItems
+      localStorage.setItem('cart', JSON.stringify(state.cartItems));
     },
     removeAllItemsFromCart: (state) => {
       state.cartItems = [];
