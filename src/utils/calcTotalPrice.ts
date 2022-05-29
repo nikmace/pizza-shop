4b5417c53bc657b7b7aa2c03b87c9f609557feae
Nikita Baranov <nikmacentosh@gmail.com>
2022-05-29 15:18:59 +0300

    Calculation now includes variations

--- src/utils/calcTotalPrice.ts
+++ src/utils/calcTotalPrice.ts
@@ -1,7 +1,10 @@
 import { CartItem } from 'types/types';
 
 const calcTotalPrice = (items: CartItem[]): number => {
-  return items.reduce((sum, obj) => obj.price * obj.count + sum, 0);
+  return items.reduce(
+    (sum: number, obj: CartItem) => obj.price * obj.variation.count + sum,
+    0
+  );
 };
 
 export default calcTotalPrice;
