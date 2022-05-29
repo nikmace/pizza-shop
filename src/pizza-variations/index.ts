3047f28caeb0fbaa53c12ba3776bb152b8229706
Nikita Baranov <nikmacentosh@gmail.com>
2022-05-29 15:15:25 +0300

    Possible pizza variations

---
+++ src/pizza-variations/index.ts
@@ -0,0 +1,48 @@
+import { VariationItem } from 'types/types';
+
+const pizzaVariations: VariationItem[] = [
+  {
+    id: 'thin-26',
+    type: 'тонкое',
+    size: 26,
+    count: 0,
+    variationPrice: 100,
+  },
+  {
+    id: 'thin-30',
+    type: 'тонкое',
+    size: 30,
+    count: 0,
+    variationPrice: 120,
+  },
+  {
+    id: 'thin-40',
+    type: 'тонкое',
+    size: 40,
+    count: 0,
+    variationPrice: 160,
+  },
+  {
+    id: 'traditional-26',
+    type: 'традиционное',
+    size: 26,
+    count: 0,
+    variationPrice: 120,
+  },
+  {
+    id: 'traditional-30',
+    type: 'традиционное',
+    size: 30,
+    count: 0,
+    variationPrice: 150,
+  },
+  {
+    id: 'traditional-40',
+    type: 'традиционное',
+    size: 40,
+    count: 0,
+    variationPrice: 190,
+  },
+];
+
+export default pizzaVariations;
