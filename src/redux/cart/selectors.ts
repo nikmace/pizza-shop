d41d02864c7be753b052c24f9fcce12b60af84df
Nikita Baranov <nikmacentosh@gmail.com>
2022-05-31 14:45:29 +0300

    Added description for redux selectors

--- src/redux/cart/selectors.ts
+++ src/redux/cart/selectors.ts
@@ -1,8 +1,13 @@
 import { RootState } from 'redux/store';
 
 // eslint-disable-next-line import/prefer-default-export
+// Select all items in a cart
 export const selectCart = (state: RootState) => state.cart.cartItems;
+
+// Select total price
 export const selectTotalPrice = (state: RootState) => state.cart.totalPrice;
+
+// Returns count of pizzas with same id
 export const selectCartItemById = (id: string) => (state: RootState) =>
   state.cart.cartItems.reduce((sum, item) => {
     if (item.id === id) {
@@ -11,9 +16,11 @@ export const selectCartItemById = (id: string) => (state: RootState) =>
     return sum;
   }, 0);
 
+// Select the total count of items in cart
 export const selectCartItemsCount = (state: RootState): number =>
   state.cart.cartItems.reduce((sum, item) => sum + item.variation.count, 0);
 
+// Returns total price for particular variation
 export const selectVariationTotalPrice =
   (id: string, variationId: string) => (state: RootState) =>
     state.cart.cartItems.reduce((sum, item) => {
