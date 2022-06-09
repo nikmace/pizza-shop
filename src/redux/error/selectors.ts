d59ab46a34d993bf4a39f99c42e4261c8b401f67
Nikita Baranov <nikmacentosh@gmail.com>
2022-06-09 12:16:38 +0300

    Prettier'd files

--- src/redux/error/selectors.ts
+++ src/redux/error/selectors.ts
@@ -3,4 +3,3 @@ import { RootState } from 'redux/store';
 
 export const selectError = (state: RootState) => state.error.message;
 export const selectStatusCode = (state: RootState) => state.error.statusCode;
-
