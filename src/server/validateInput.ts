c90d4de3cb52bf4f6d324ddbcfa9fa3a3fc07594
Nikita Baranov <nikmacentosh@gmail.com>
2022-06-12 17:38:29 +0300

    Added function to validate form input from server

---
+++ src/server/validateInput.ts
@@ -0,0 +1,47 @@
+import { PayloadAction } from '@reduxjs/toolkit';
+import { Dispatch } from 'react';
+
+import { setError } from 'redux/error/slice';
+import { InputValues } from 'pages/Checkout';
+
+export type ValidationResponse = {
+  message: string;
+  success: boolean;
+  errors: {
+    firstName: string[];
+    lastName: string[];
+    email: string[];
+    phone: string[];
+    address: string[];
+    time: string[];
+  };
+  errorCount: number;
+};
+
+const validateInput = async (
+  input: InputValues,
+  dispatch: Dispatch<PayloadAction<string>>
+): Promise<ValidationResponse | undefined> => {
+  try {
+    const data = await fetch('https://pizz-server.herokuapp.com/api/v1/order', {
+      method: 'POST',
+      body: JSON.stringify(input),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const parsedData: ValidationResponse = await data.json();
+
+    return parsedData;
+  } catch (err) {
+    if (err instanceof Error) {
+      dispatch(setError(err.message));
+    }
+  }
+
+  return undefined;
+};
+
+export default validateInput;
