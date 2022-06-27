7e038d0386d6fedeed85bb31c163dec83803cec8
Nikita Baranov <nikmacentosh@gmail.com>
2022-06-27 22:34:10 +0300

    Removed notification provider from unused library

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { NotificationProvider } from 'web3uikit';
 
 import { store } from 'redux/store';
 
@@ -16,11 +15,9 @@ if (!rootElement) throw new Error('Failed to find the root element');
 const root = createRoot(rootElement);
 
 root.render(
-  <NotificationProvider>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
-  </NotificationProvider>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
