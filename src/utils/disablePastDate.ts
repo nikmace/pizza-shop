a95f76a6b7dbe00a39e19abd8a27c17f01479054
nikmace <nikmacentosh@gmail.com>
2022-07-26 20:17:56 +0300

    Added 45 min to account for delivery time

--- src/utils/disablePastDate.ts
+++ src/utils/disablePastDate.ts
@@ -1,4 +1,13 @@
-// eslint-disable-next-line import/prefer-default-export
+/* eslint-disable import/prefer-default-export */
+/**
+ * @description Calculate current date in the following format: yyyy-MM-ddThh:mm,
+ * where minutes are increased by 30 min at least to account for
+ * processing the order and delivery.
+ * @params None
+ * @returns Current date
+ */
 export const disablePastDate = () => {
-  return new Date().toISOString().slice(0, 16);
+  return new Date(new Date().getTime() + 225 * 60000)
+    .toISOString()
+    .slice(0, 16);
 };
