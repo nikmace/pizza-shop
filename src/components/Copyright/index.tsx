2733869a22a05e9680a21f41fd81d3475a30f243
nikmace <nikmacentosh@gmail.com>
2022-08-04 15:05:58 +0300

    Copyright component

---
+++ src/components/Copyright/index.tsx
@@ -0,0 +1,11 @@
+const Copyright = () => {
+  return (
+    <p
+      style={{ textAlign: 'center', marginBottom: '10px', marginTop: '-20px' }}
+    >
+      All rights reserved © Dodo Pizza
+    </p>
+  );
+};
+
+export default Copyright;
